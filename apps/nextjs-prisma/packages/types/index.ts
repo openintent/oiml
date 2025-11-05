6cffcb39da65152c3fea46a6c1b381f94833f341
oimladmin <admin@oiml.dev>
2025-11-05 11:44:09 -0800

    Revert changes

--- apps/nextjs-prisma/packages/types/index.ts
+++ apps/nextjs-prisma/packages/types/index.ts
@@ -4,13 +4,3 @@ export interface ErrorResponse {
   error: string;
 }
 
-// User entity
-export interface UserInterface {
-  id: string;
-  email: string;
-}
-
-export interface UserResponse {
-  data: UserInterface | UserInterface[];
-}
-
